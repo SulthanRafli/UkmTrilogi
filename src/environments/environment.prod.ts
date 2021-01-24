e55a424d73d0d11ce5ec70533977e941ac836b9f
sulthan <msulthanraflim@gmail.com>
2021-01-24 12:50:36 +0700

    Update File

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAt13hHssIGIdUix7jP5LqFeC3KHnmT6Aw",
+    authDomain: "projectukm-dc33e.firebaseapp.com",
+    projectId: "projectukm-dc33e",
+    storageBucket: "projectukm-dc33e.appspot.com",
+    messagingSenderId: "1071005979542",
+    appId: "1:1071005979542:web:65567a768e9ec932b503b7"
+  }
 };
